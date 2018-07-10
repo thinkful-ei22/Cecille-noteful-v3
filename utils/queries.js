cc1636c3554ecbf60e99f1dee997cb9370b59abf
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-10 13:01:32 -0700

    Created Note.findByIdAndUpdate query - test successful

--- utils/queries.js
+++ utils/queries.js
@@ -15,10 +15,14 @@ mongoose.connect(MONGODB_URI)
     }
     // return Note.findById('5b450cfeabfbfc3356ad0017');
     //return Note.find(filter).sort({ updatedAt: 'desc' });
-    return Note.create({
-      title: 'My Shiba Miko',
-      content: 'Has a Curly Tail'
-    })
+    // return Note.create({
+    //   title: 'My Shiba Miko',
+    //   content: 'Has a Curly Tail'
+    // })
+    return Note.findByIdAndUpdate('5b450cfeabfbfc3356ad0017',
+      { title: 'My Labrador Max', content: 'Has a Straight Tail'},
+      { new: true }
+    )
   })
   .then(results => {
     console.log(results);
