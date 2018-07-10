8eb58482dcc2e6db68d52c7b9168de2f5570e903
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-10 12:22:26 -0700

    Implemented test query to find Lady Gaga note in queries.js - test returned appropriate note

---
+++ utils/queries.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const { MONGODB_URI } = require('../config');
+
+const Note = require('../models/note');
+
+console.log(MONGODB_URI);
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    const searchTerm = 'Lady Gaga';
+    let filter = {};
+
+    if (searchTerm) {
+      filter.title = { $regex: searchTerm };
+    }
+    
+    return Note.find(filter).sort({ updatedAt: 'desc' });
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect()
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
