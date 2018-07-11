addc1d846a5f4c61e92f8ab7fa8d160accdd9c0d
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-11 12:21:40 -0600

    Removed additional test in scripts in package.json

--- test/notes.test.js
+++ test/notes.test.js
@@ -11,22 +11,3 @@ const seedNotes = require('../db/seed/notes');
 
 const expect = chai.expect;
 chai.use(chaiHttp);
-
-describe('Hook Functions', function() {
-  before(function () {
-    return mongoose.connect(TEST_MONGODB_URI)
-      .then(() => mongoose.connection.db.dropDatabase());
-  });
-
-  beforeEach(function () {
-    return Note.insertMany(seedNotes);
-  });
-
-  afterEach(function () {
-    return mongoose.connection.db.dropDatabase();
-  });
-
-  after(function () {
-    return mongoose.disconnect();
-  });
-});
