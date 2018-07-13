7bdb8a095aa36eafa9bdd4625e129dfccc8cf612
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-13 11:10:02 -0400

    Created GET ALL tags endpoint sort asc - POSTMAN test passed

--- routes/tags.js
+++ routes/tags.js
@@ -8,4 +8,15 @@ const Tag = require('../models/tags');
 
 const mongoose = require('mongoose');
 
+/* ========== GET/READ ALL ITEMS ========== */
+router.get('/', (req, res, next) => {
+  Tag
+  .find()
+  .sort({ name: 'asc' })
+  .then(tags => res.status(200).json(tags))
+  .catch(err => {
+    next(err);
+  })
+});
+
 module.exports = router;
