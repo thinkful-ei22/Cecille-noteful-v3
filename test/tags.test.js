b4be6b2abf5e04a3d74fbae92320033dc1a8e4e7
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-13 14:54:39 -0400

    Created tags.test.js in test folder and setup initial configuration

---
+++ test/tags.test.js
@@ -0,0 +1,33 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const mongoose = require('mongoose');
+
+const app = require('../server');
+const { TEST_MONGODB_URI } = require('../config');
+
+const Tag = require('../models/tags');
+
+const seedTags = require('../db/seed/tags');
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('Hook Functions', function() {
+  before(function () {
+    return mongoose.connect(TEST_MONGODB_URI)
+      .then(() => mongoose.connection.db.dropDatabase());
+  });
+
+  beforeEach(function () {
+    return Tag.insertMany(seedTags);
+  });
+
+  afterEach(function () {
+    return mongoose.connection.db.dropDatabase();
+  });
+
+  after(function () {
+    return mongoose.disconnect();
+  });
+
+});
