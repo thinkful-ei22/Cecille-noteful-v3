d02e7a29eb24b21124f8a2fa589abf45c28f6175
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-13 11:03:22 -0400

    Created tags.js in routes folder and setup initial configuration

---
+++ routes/tags.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+
+const router = express.Router();
+
+const Tag = require('../models/tags');
+
+const mongoose = require('mongoose');
+
+module.exports = router;
