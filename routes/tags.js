1302cf6c9af0ac251dac684df815b803f56b5db5
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-13 11:14:43 -0400

    Created GET tags by ID endpoint w/validation - POSTMAN test passed

--- routes/tags.js
+++ routes/tags.js
@@ -19,4 +19,24 @@ router.get('/', (req, res, next) => {
   })
 });
 
+/* ========== GET/READ A SINGLE ITEM ========== */
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('Id is not valid!');
+    err.status = 400;
+    return next(err);
+  }
+
+  Tag
+  .findById(id)
+  .then(tag => {
+    res.status(200).json(tag)
+  })
+  .catch(err => {
+    next(err);
+  })
+});
+
 module.exports = router;
